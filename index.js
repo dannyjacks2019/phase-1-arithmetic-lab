0928844cad10785c7155ad15720f210374dc0a2a
Dan Jackson <dannyjacks2019@gmail.com>
2023-04-29 11:46:30 -0400

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+    let num1 = 31
+    let num2 = 2
+    let multiply = (num1 * num2)
+
+    let num9 = 2
+    let random = (num9)
+
+    let num4 = 3
+    let num3 = 12
+    let mod =(num3 / num4)
+let max = (1, 5, 10, 15, 20)
+Math.max(1, 5, 10, 15, 20)
 
 // Write your code here
